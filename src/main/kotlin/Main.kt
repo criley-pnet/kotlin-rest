5b4e9465530eaf0568fd57158e8fa71b7b1350be
Chris Riley <criley@peoplenetonline.com>
2018-05-04 14:03:18 -0400

    configure logging

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,14 @@
 
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.slf4j.event.Level
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, 8080) {
@@ -16,6 +19,9 @@ fun main(args: Array<String>) {
             get ("/demo") {
                 call.respondText("HELLO WORLD!")
             }
+        }
+        install(CallLogging) {
+            level = Level.TRACE
         }
     }
     server.start(wait = true)
